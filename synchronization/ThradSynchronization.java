7849e2a8081ce8fa4ecbcd864a66d53a88a45d51
Gummadi-shirisha12 <100821811+Gummadi-shirisha12@users.noreply.github.com>
2022-03-03 15:22:43 +0530

    Synchronization

---
+++ synchronization/ThradSynchronization.java
@@ -0,0 +1,24 @@
+package synchronization;
+
+public class ThradSynchronization {
+	 public static void main(String args[]) 
+	    { 
+	        Sender snd = new Sender(); 
+	        ThreadedSend S1 = 
+	            new ThreadedSend( " Hi " , snd ); 
+	        ThreadedSend S2 = 
+	            new ThreadedSend( " Bye " , snd ); 
+	        S1.start(); 
+	        S2.start(); 
+	        try
+	        { 
+	            S1.join(); 
+	            S2.join(); 
+	        } 
+	        catch(Exception e) 
+	        { 
+	            System.out.println("Interrupt"); 
+	        } 
+	    } 
+
+}
