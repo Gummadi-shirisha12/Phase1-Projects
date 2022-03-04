235a9b92aa7a75dfa06a3e75b339856dfaa2f4d8
Gummadi-shirisha12 <100821811+Gummadi-shirisha12@users.noreply.github.com>
2022-03-04 11:22:15 +0530

    Order Statistics

---
+++ orderstatistcs/User.java
@@ -0,0 +1,14 @@
+package orderstatistcs;
+
+public class User {
+	public static void main(String[] args) {
+		Statistics ob = new Statistics(); 
+        int arr[] = {12, 3, 5, 7, 4, 19, 26}; 
+        int n = arr.length,k = 4; 
+        System.out.println("K'th smallest element is "+ ob.kthSmallest(arr, 0, n-1, k)); 
+    }
+}
+
+	
+
+
